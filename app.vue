9f8254fa833f426db74629e3768fe078a9c17e46
Alvaro Dwi <varoa.dwi@gmail.com>
2023-10-30 00:00:01 +0700

    final

--- app.vue
+++ app.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="font-serif w-60% mx-auto my-4">
+  <div class="font-serif w-50% mr-auto ml-4 my-4">
     <div id="header">
       <h1>Klasterisasi Tweet mengenai Indihome</h1>
       <p>Data Mining Universitas Padjadjaran 2023</p>
@@ -13,67 +13,99 @@
       </div>
     </div>
 
-    <h2>Overview dari Project</h2>
-    <p>Lorem ipsum</p>
-
-    <h2>Tahapan Pengerjaan</h2>
-    <h3>Pengumpulan Data</h3>
-    <p>Lorem</p>
-
-    <h3>Pemrosesan Data</h3>
-    <p>Lorem</p>
-
-    <h3>Metode Klasterisasi</h3>
-    <p>Lorem</p>
-
-    <h2>Hasil Project</h2>
-
     <h3>Hasil Klaster</h3>
-    <p>Berikut adalah sampel dari klustering tweet yang telah kami lakukan</p>
-    <table>
-      <thead>
+    <p>
+      Berikut adalah sampel dari klastering tweet yang telah kami lakukan.
+      Dibuat 4 klaster berdasarkan hasil analisis sentimen yaitu : positif,
+      negatif, netral, dan gabungan atau <i>compound</i>.
+    </p>
+    <p>
+      Suatu tweet dikatakan masuk ke dalam klaster ketika nilai pada klaster
+      tersebut lebih tinggi dibanding klaster lainnya. Contohnya, data pertama
+      masuk ke klaster netral, data kedua masuk ke klaster negative, dan
+      seterusnya.
+    </p>
+    <table class="w-full text-left text-gray-600">
+      <thead class="text-sm text-gray-700 uppercase bg-gray-50">
         <tr>
-          <th>positive</th>
-          <th>neutral</th>
-          <th>negative</th>
-          <th>compound</th>
+          <th class="px-4 py-2">positive</th>
+          <th class="px-4 py-2">neutral</th>
+          <th class="px-4 py-2">negative</th>
+          <th class="px-4 py-2">compound</th>
         </tr>
       </thead>
       <tbody>
         <tr v-for="(r, index) in result" :key="index">
-          <td>{{ r.positive }}</td>
-          <td>{{ r.neutral }}</td>
-          <td>{{ r.negative }}</td>
-          <td>{{ r.compound }}</td>
+          <td
+            class="px-4 py-2"
+            :class="r.cluster == 'positive' ? 'font-bold' : ''"
+          >
+            {{ r.positive }}
+          </td>
+          <td
+            class="px-4 py-2"
+            :class="r.cluster == 'neutral' ? 'font-bold' : ''"
+          >
+            {{ r.neutral }}
+          </td>
+          <td
+            class="px-4 py-2"
+            :class="r.cluster == 'negative' ? 'font-bold' : ''"
+          >
+            {{ r.negative }}
+          </td>
+          <td
+            class="px-4 py-2"
+            :class="r.cluster == 'compound' ? 'font-bold' : ''"
+          >
+            {{ r.compound }}
+          </td>
+        </tr>
+        <tr>
+          <td class="px-4 py-2">...</td>
+          <td class="px-4 py-2">...</td>
+          <td class="px-4 py-2">...</td>
+          <td class="px-4 py-2">...</td>
         </tr>
       </tbody>
     </table>
 
-    <h3>Statistik Klustering</h3>
-    <ClusterLollipopChart :data="cluster" :width="960" :height="500" />
-    <table>
-      <thead>
-        <tr>
-          <th>Cluster</th>
-          <th>Total</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="(c, index) in cluster" :key="index">
-          <td>{{ c.cluster }}</td>
-          <td>{{ c.count }}</td>
-        </tr>
-      </tbody>
-    </table>
+    <h3>Statistik klastering</h3>
+    <div class="flex flex-row">
+      <div>
+        <ClusterLollipopChart :data="cluster" :width="960" :height="500" />
+      </div>
+      <div>
+        <table class="w-60% text-left text-gray-600">
+          <thead class="text-sm text-gray-700 uppercase bg-gray-50">
+            <tr>
+              <th class="px-4 py-2">Cluster</th>
+              <th class="px-4 py-2">Total</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="(c, index) in cluster" :key="index">
+              <td class="px-4 py-2">{{ c.cluster }}</td>
+              <td class="px-4 py-2">{{ c.count }}</td>
+            </tr>
+            <tr class="bg-gray-100 font-bold">
+              <td class="px-4 py-2">Total</td>
+              <td class="px-4 py-2">{{ totalCluster }}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
 
     <h3>Wordcloud</h3>
-
-    <Cloud
-      class="border border-solid border-black"
-      :words="words"
-      :width="960"
-      :height="500"
-    />
+    <div class="absolute">
+      <Cloud
+        class="mb-4 mx-auto border border-solid border-black"
+        :words="words"
+        :width="960"
+        :height="500"
+      />
+    </div>
   </div>
 </template>
 
@@ -89,6 +121,7 @@ useHead({
 const words = ref([]);
 const result = ref([]);
 const cluster = ref([]);
+const totalCluster = ref(0);
 
 async function resolveCsv() {
   const wordsCsv = await d3.csv("/dataset/words_dict.csv", (d) => {
@@ -99,7 +132,13 @@ async function resolveCsv() {
   });
 
   const resultCsv = await d3.csv("/dataset/result_sample.csv");
-  const clusterCsv = await d3.csv("/dataset/cluster.csv");
+  const clusterCsv = await d3.csv("/dataset/cluster.csv", (d) => {
+    totalCluster.value += +d.count;
+    return {
+      cluster: d.cluster,
+      count: d.count,
+    };
+  });
 
   words.value = wordsCsv
     .sort((word1, word2) => word2.size - word1.size)
